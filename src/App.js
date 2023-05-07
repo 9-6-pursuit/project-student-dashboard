5faa85029727320c530e242a0dd65dc0b86d6015
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-05-07 14:54:47 -0400

    more structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
-import Students from './Components/Students';
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import Students from "./Components/Students";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Sidebar />
-      <Students />
+    <div className="student-dashboard-container">
+      <div className="hearder-row">
+        <div className="header">
+          <Header />
+        </div>
+      </div>
+      <div className="bottom-row">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="students">
+          <Students />
+        </div>
+      </div>
     </div>
   );
 }
