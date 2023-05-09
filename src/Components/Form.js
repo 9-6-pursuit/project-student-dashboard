ea96ba43ec8b40c3251101452e9289718d7c8694
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-05-09 16:05:12 -0400

    project-student-dashboard9.6

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,1 +1,30 @@
-import React from "react";
+import React from "react"
+const Form = () => {
+    return(
+        <fieldset>
+            <div>
+                <h2>1-on-1 Notes</h2>
+                <form>
+                    <label>
+                        Commenter Name 
+                         <input type ="text" name="commenter"></input>
+                    </label>
+                    <br/>
+                    <label>
+                        Comments
+                        <input type="text" name="commenttext"></input>
+                    </label>
+                    <br/>
+                    <button type="submit">Add Note</button>
+                </form>
+
+
+
+
+            </div>
+        </fieldset>
+    )
+
+}
+
+export default Form;
