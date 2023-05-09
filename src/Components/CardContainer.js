f58e4bef060baf740ce406f569171dbf6a42c4bb
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-05-09 15:55:48 -0400

    project-student-dashboard9.6

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -8,6 +8,23 @@ const CardContainer =({data, heading}) => {
 
         <ul className="container">
             <h2>{heading}</h2>
+            <p>Total Students:{data.legth}</p>
+            {data.map(({names, profilePhoto, id,username, dob, certifications, codewars, cohort}) => }
+
+            return (
+                <div key={id}>
+                 <Studentcard
+                 names={names}
+                 profilePhoto ={profilePhoto}
+                 username = {username}
+                 dob={dob}
+                 certifications={certifications}
+                 codewars={codewars}
+                 cohort={cohort}
+                 />
+                </div>
+            );
+            })}
         </ul>
     )
 }
