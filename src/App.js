6be1409fe745c556f2cfbe0c9e8b60ea47020230
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-10 16:50:40 -0400

    add cohort list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+import { useState } from "react";
+import data from "./data/data.json";
+
 import CohortList from "./components/CohortList";
 import Header from "./components/Header";
 import StudentsList from "./components/StudentsList";
-import data from "./data/data.json";
 
 import "./App.css"
 
 function App() {
+
+  const [displayedStudents, setDisplayedStudents] = useState(data)
+
+  const getCohorts = (studentsList) => {
+    let cohortCounter = {}
+    studentsList.forEach(student => {
+      cohortCounter[student.cohort.cohortCode] = true
+    });
+    return Object.keys(cohortCounter)
+  }
+
+
+  const filterStudentsByCohort = (cohortCode) => {
+    let filteredList = data.filter(student => {
+      return student.cohort.cohortCode === cohortCode
+    })
+    setDisplayedStudents(filteredList)
+    return filteredList
+  }
+
+  // console.log(filterStudentsByCohort("Winter2025"))
+
   return (
     <div>
       <Header />
-      <section class="main-page-content" >
-        <CohortList />
-        <StudentsList students={data}/>
+      <section className="main-page-content" >
+        <CohortList 
+          filterStudentsByCohort={filterStudentsByCohort} 
+          cohorts={getCohorts(data)}  
+        />
+        <StudentsList students={displayedStudents}/>
       </section>
       
     </div>
--- src/components/CohortList.js
+++ src/components/CohortList.js
@@ -1,7 +1,20 @@
-export default function CohortList (){
+import "./CohortList.css"
+export default function CohortList ({filterStudentsByCohort, cohorts}){
     return (
-        <div>
-            This is the cohort list.
-        </div>
+        <ul className="cohort-list">
+        <div>This is the cohort list.</div>
+        <li>Number of cohorts: {cohorts.length}</li>
+        {cohorts.map((cohort) => {
+            return (
+                <li 
+                className="list"
+                key ={cohort} 
+                onClick={() => {
+                    filterStudentsByCohort(cohort)
+                }}
+                >{cohort.substring(0, cohort.length - 4) + " " + cohort.substring(cohort.length - 4, cohort.length)}</li>
+            )
+        })}
+        </ul>
     )
 }
