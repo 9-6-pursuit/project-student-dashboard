67c21a2522165b2aed2f478aa20abda90c37ef54
paoAracena <Paolaaracena1233@gmail.com>
2023-05-10 11:53:52 -0400

    fixed css

--- src/Components/CohortList.js
+++ src/Components/CohortList.js
@@ -2,19 +2,27 @@ import "./CohortList.css"
 
 function CohortList ({cohorts , filterStudentsByCohort}) {
 
-    return (
+  return (
       <div class="cohort-List ">
         <h3> Choose a class by start date</h3>
-<ul className="cohort-graph">
-    <li> All Students</li>
-{ cohorts.map((cohort) => {
-            return (
-              <li key={cohort} onClick={() => {
+
+
+    <ul  class="cohort-graph">
+    <li  className="dem-cohorts"> All Students</li>
+                { cohorts.map((cohort) => { return (
+
+                <li 
+                className="dem-cohorts" key={cohort} onClick={() => {
                 filterStudentsByCohort(cohort)
-              }}>{cohort.substring(0, cohort.length - 4) + " " + cohort.substring(cohort.length - 4, cohort.length)}</li>
+              }}>{cohort.substring(0, cohort.length - 4) + " " + cohort.substring(cohort.length - 4, cohort.length)}
+              </li>
             )
         })}
         </ul>
+
+
+
+
       </div>
     );
   }
