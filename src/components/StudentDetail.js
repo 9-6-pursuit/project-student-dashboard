15152feef8c2201b616b47f6f183a5783f5c02cb
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-05-11 01:59:09 -0400

    Added Notes Form

--- src/components/StudentDetail.js
+++ src/components/StudentDetail.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 function StudentDetail({ student }) {
+  const [notes, setNotes] = useState([...student.notes]);
 
   let goalPercentage = Math.floor(student.codewars.current.total / student.codewars.goal.total * 100);
 
-  const getSign = isTrue => isTrue ? "✅" : "❌"
+  const getSign = isTrue => isTrue ? "✅" : "❌";
+
+  let id = 100;
+
+  const handleAddNote = (event) => {
+    event.preventDefault();
+
+    let name = document.getElementById("commenter").value;
+    let comment = document.getElementById("comment").value;
+    document.getElementById("note-form").reset();
+
+    setNotes([...notes, {"commenter" : name, "comment" : comment}]);
+    // console.log(name, comment)
+  }
 
   return (
+    <>
     <div className="student-detail">
       <div className="individual-detail codewars">
         <h4>Codewars</h4>
@@ -27,6 +44,26 @@ function StudentDetail({ student }) {
         <p><span>Github:</span> {getSign(student.certifications.github)}</p>
       </div>
     </div>
+    <div className="notes">
+      <h4>1-on-1 Notes</h4>
+      <form id="note-form" onSubmit={handleAddNote}>
+        <div className="form-element commenter">
+          <label>Commenter Name</label>
+          <input type="text" id="commenter" required/>
+        </div>
+        <div className="form-element comment">
+          <label>Comment</label>
+          <input type="text" id="comment" required/>
+        </div>
+        <div className="form-element">
+          <button>Add Note</button>
+        </div>
+      </form>
+    </div>
+    <ul>
+      {notes.map(note => <li key={id++}>{note.commenter} says, "{note.comment}"</li>)}
+    </ul>
+    </>
   );
 }
 
