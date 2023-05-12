27fe752d12f025a26e616c5af154dd22a458e424
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-12 13:56:54 -0400

    Done I think I couldn't get the dropdown for all of the class by start dates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import Header from './Components/Header.js'
+import SideBar from './Components/SideBar.js'
+import Students from './Components/Student-List.js'
+import studentData from './data/data.json'
+import { useState } from "react"
+import "./index.js"
 
 function App() {
+
+
+  const [filterCohort, setFilterCohort] = useState(studentData)
+
+  function getCohorts() {
+    return studentData.map((student) => {
+      return student.cohort.cohortCode
+    })
+
+  }
+
+  function filterStudentsByCohort(cohort) {
+    console.log(cohort)
+
+    let filteredData = studentData.filter((student) => {
+        return student.cohort.cohortCode === cohort
+    })
+    setFilterCohort(filteredData)
+
+  }
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
+
+  <>
+    <Header />
+    <div className="page-content">
+        <SideBar cohorts={getCohorts()} filterStudentsByCohort={filterStudentsByCohort}/>
+        <Students studentData={filterCohort}/>
+
     </div>
+
+    </>
   );
 }
 
